31a23891f59776eb0b5e1f455f83e4b3bc01249e
Kirill <kirillnaumenk0@mail.ru>
2022-08-10 19:48:46 +0600

    №1

---
+++ №1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите тёхзначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+if(a > 100)
+{
+    if(a < 900)
+    {
+     int b = a % 100;
+     b/= 10;
+     Console.WriteLine(b);
+    }
+    else
+    {
+    Console.WriteLine("Вы ввели неверное число");
+    }
+}
+else
+{
+Console.WriteLine("Вы ввели неверное число");
+}
