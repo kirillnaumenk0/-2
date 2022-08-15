c7981675cc5d3dcb00ff9522b6bb4c42fdaf2c0b
Kirill <kirillnaumenk0@mail.ru>
2022-08-15 21:04:40 +0600

    commit№2

---
+++ №2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+string num = Console.ReadLine();
+if(num.Length < 3)
+{
+Console.WriteLine("Третья цифра отсутствует");
+}
+else
+{
+if(Convert.ToInt32(num) < 0)
+{
+Console.WriteLine("Третья цифра: " + num[3]);
+} 
+else
+{
+Console.WriteLine("Третья цифра: " + num[2]);
+}
+}
