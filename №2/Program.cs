25f63c4153cc1bdddf8921eef7e7fc97c21ad24e
Kirill <kirillnaumenk0@mail.ru>
2022-08-15 21:09:31 +0600

    commit№2правка

--- №2/Program.cs
+++ №2/Program.cs
@@ -1,17 +1,17 @@
 ﻿Console.WriteLine("Введите число");
 string num = Console.ReadLine();
-if(num.Length < 3)
+if (num.Length < 3)
 {
-Console.WriteLine("Третья цифра отсутствует");
+    Console.WriteLine("Третья цифра отсутствует");
 }
 else
 {
-if(Convert.ToInt32(num) < 0)
-{
-Console.WriteLine("Третья цифра: " + num[3]);
-} 
-else
-{
-Console.WriteLine("Третья цифра: " + num[2]);
-}
+    if (Convert.ToInt32(num) < 0)
+    {
+        Console.WriteLine("Третья цифра: " + num[3]);
+    }
+    else
+    {
+        Console.WriteLine("Третья цифра: " + num[2]);
+    }
 }
