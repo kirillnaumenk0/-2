6318ed64dcdd271d44dca6b40c8710bfec4eacad
Kirill <kirillnaumenk0@mail.ru>
2022-08-15 21:25:13 +0600

    commit№3

--- №3/Program.cs
+++ №3/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿string[] Week = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье", };
+Console.WriteLine("Введите день недели");
+for (int i = 0; i < Week.Length; i++)
+{
+    Console.WriteLine(i + 1 + " - " + Week[i] + ", ");
+}
+Console.WriteLine(" ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day == 6 || day == 7)
+{
+    Console.WriteLine("да" + day + " - выходной!");
+}
+else
+{
+    Console.WriteLine("нет" + day + " - будни:(");
+}
